fbb5d87972fa0e87d653a04fcd0296172d55c13e
Avneet Kaur <avkaur79@gmail.com>
2017-03-31 13:50:43 +0530

    DTDH

--- app/src/main/java/com/example/yash/intellih_initial_ui/CustomAdapter.java
+++ app/src/main/java/com/example/yash/intellih_initial_ui/CustomAdapter.java
@@ -1,10 +1,65 @@
 package com.example.yash.intellih_initial_ui;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Admin on 31-03-2017.
  */
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+
+    private ArrayList<DataModel> dataSet;
+
+
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView roomName;
+        ImageView imageViewIcon;
+
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            this.roomName = (TextView) itemView.findViewById(R.id.room_name);
+            this.imageViewIcon = (ImageView) itemView.findViewById(R.id.room_icon);
+        }
+    }
+
+
+    @Override
+    public CustomAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_layout, parent, false);
+
+        view.setOnClickListener(MainActivity.myOnClickListener);
+
+        MyViewHolder myViewHolder = new MyViewHolder(view);
+        return myViewHolder;
+
+    }
+    public CustomAdapter(ArrayList<DataModel> data) {
+        this.dataSet = data;
+    }
+
+    @Override
+    public void onBindViewHolder(CustomAdapter.MyViewHolder holder, int position) {
+        TextView textViewName = holder.roomName;
+        ImageView imageView = holder.imageViewIcon;
+
+        textViewName.setText(dataSet.get(position).getRoom());
+        imageView.setImageResource(dataSet.get(position).getImage());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSet.size();
+    }
 }
--- app/src/main/java/com/example/yash/intellih_initial_ui/MainActivity.java
+++ app/src/main/java/com/example/yash/intellih_initial_ui/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.yash.intellih_initial_ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
@@ -19,5 +21,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        data = new ArrayList<DataModel>();
+        addData();
+        adapter = new CustomAdapter(data);
+        recyclerView.setAdapter(adapter);
+    }
+
+    void addData(){
+        data.add(new DataModel("Bed Room",R.drawable.kid));
+        data.add(new DataModel("Drawing Room",R.drawable.kid));
+        data.add(new DataModel("Kids Room",R.drawable.kid));
+        data.add(new DataModel("Kitchen",R.drawable.kid));
     }
 }
